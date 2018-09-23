462eae5c23824fbb5aebba634f9b669b36e603eb
John Olubori David <daveholuborhee@gmail.com>
2018-09-23 02:11:30 +0100

    Add path.join to serve static

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var path = require('path');
 var serveStatic = require('serve-static');
 app = express();
-app.use(serveStatic(__dirname + "/dist"));
+app.use(serveStatic(path.join(__dirname, '/dist')))
 var port = process.env.PORT || 5001;
 app.listen(port);
 console.log('server started '+ port);
