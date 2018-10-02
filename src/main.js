e6e138703b6c8293e294b4a39fe4cdd5714dad0d
Daltimore <obinnaleo78@gmail.com>
2018-10-02 13:42:13 +0100

    fix vue analytics plugin

--- src/main.js
+++ src/main.js
@@ -13,8 +13,7 @@ import 'iview/dist/styles/iview.css';
 Vue.use(VueRouter);
 Vue.use(VueAnalytics, {
   id: 'UA-126813609-1',
-  router,
-  checkDuplicatedScript: true
+  router
 })
 Vue.config.productionTip = false
 
