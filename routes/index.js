f6dfb2bc07ea0c1582f28c76bbcf462c12dbfd07
Habeeb Mac-iver <habeeb.maciver@aiesec.net>
2018-11-22 11:10:22 +0100

    Changed /login route for consumers login component

--- routes/index.js
+++ routes/index.js
@@ -129,10 +129,10 @@ const routes = [
   },
   { path: '/web', component: BaseConsumer, // All pages for the new user features should reside here
     children: [
-      { path: '/login', component: ConsumerSignIn },
-      { path: 'country', component: SelectCountry }
+      { path: '/country', component: SelectCountry }
     ]
   },
+  { path: '/login', component: ConsumerSignIn },
   { path: "*", component: NotFound }
 ]
 
