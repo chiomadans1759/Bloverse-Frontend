b6afdfb6ff0360ab6fead26d407de189aa191f20
John Olubori David <daveholuborhee@gmail.com>
2018-12-01 02:12:58 +0100

    Update route/index.js

--- routes/index.js
+++ routes/index.js
@@ -29,8 +29,8 @@ import AdminHome from '../src/views/admin/Home.vue';
 
 import BaseFeeds from '../src/layouts/BaseFeeds.vue';
 import PostFeeds from '../src/views/PostFeeds.vue';
-import PostDisplay from '../src/views/PostDisplay.vue';
-
+import PostDisplay from '../src/views/PostDisplay.vue'; 
+ 
 import ConsumerSignIn from '../src/views/consumers/ConsumerSignIn.vue';
 import SelectCountry from '../src/views/consumers/SelectCountry.vue';
 import SelectCategory from '../src/views/consumers/SelectCategory.vue';
@@ -39,41 +39,25 @@ import ConsumerLandingPage from '../src/views/consumers/ConsumerLandingPage.vue'
 import ConsumersTrending from '../src/views/consumers/ConsumersTrending.vue';
 import ConsumerProfile from '../src/views/consumers/ConsumerProfile.vue';
 
-const routes = [{
-    path: '/',
-    component: BaseFeeds,
-    children: [{
-        path: '',
-        component: PostFeeds
-      },
-      {
-        path: 'posts',
-        redirect: '/'
-      },
-      // { path: 'posts/:slug', component: PostDisplay }
+const routes = [
+  {
+    path: '/', component: BaseFeeds,
+    children: [ 
+      { path: '', component: PostFeeds },
+      { path: 'posts', redirect: '/' },
+      { path: 'posts/:slug', component: PostDisplay }
     ]
   },
-
+  
   {
     path: '/creators',
     component: BlankBase,
     children: [
-      //   { path: 'new', component: JournalistLanding },
-      {
-        path: '/creators',
-        component: NewJournalistLanding
-      },
-      {
-        path: 'apply',
-        component: JournalistApply
-      },
-      {
-        path: 'login',
-        component: JournalistSignIn
-      },
-      {
-        path: 'register',
-        component: JournalistSetUp,
+    //   { path: 'new', component: JournalistLanding },
+      { path: '/creators', component: NewJournalistLanding},
+      { path: 'apply', component: JournalistApply },
+      { path: 'login', component: JournalistSignIn },
+      { path: 'register', component: JournalistSetUp,
         beforeEnter(to, from, next) {
           if (store.getters.isAllowedToRegister) {
             next()
@@ -82,9 +66,7 @@ const routes = [{
           }
         },
       },
-      {
-        path: 'setup',
-        component: JournalistManualSetUp,
+      { path: 'setup', component: JournalistManualSetUp,
         beforeEnter(to, from, next) {
           if (store.getters.isAllowedToRegister) {
             next()
@@ -93,116 +75,46 @@ const routes = [{
           }
         }
       },
-      {
-        path: 'verify',
-        component: JournalistVerify
-      },
-      {
-        path: ':username',
-        component: BaseDashBoard,
-        meta: {
-          journalist: true,
-          auth: true
-        },
-        children: [{
-            path: '',
-            component: MyProfile
+      { path: 'verify', component: JournalistVerify },
+      { path: ':username', component: BaseDashBoard, meta: { journalist: true, auth: true },
+        children: [
+          { path: '', component: MyProfile
           },
-          {
-            path: 'dashboard',
-            component: DashBoardHome
-          },
-          {
-            path: 'posts',
-            component: BlankBase,
-            children: [{
-                path: '',
-                component: MyPosts
-              },
-              {
-                path: 'create',
-                component: CreatePost
-              },
-              {
-                path: ':slug/edit',
-                component: CreatePost
-              }
+          { path: 'dashboard', component: DashBoardHome },
+          { path: 'posts', component: BlankBase,
+            children: [
+              { path: '', component: MyPosts },
+              { path: 'create', component: CreatePost },
+              { path: ':slug/edit', component: CreatePost }
             ]
           }
         ]
       },
     ]
   },
-  {
-    path: '/faq/:person',
-    component: FrequentlyAskedQuestions
-  },
-  {
-    path: '/rules/:person',
-    component: HouseRules
-  },
-  {
-    path: '/guides',
-    component: PublishGuide
-  },
-  {
-    path: '/ranking/:person',
-    component: RankingSystem
-  },
-  {
-    path: '/admin',
-    component: BlankBase,
-    children: [{
-        path: '',
-        redirect: 'dashboard'
-      },
-      {
-        path: 'dashboard',
-        component: AdminHome
-      },
-      {
-        path: 'login',
-        component: AdminLogin
-      }
+  { path: '/faq/:person', component: FrequentlyAskedQuestions },
+  { path: '/rules/:person', component: HouseRules },
+  { path: '/guides', component: PublishGuide },
+  { path: '/ranking/:person', component: RankingSystem },
+  { path: '/admin', component: BlankBase,
+    children: [
+      { path: '', redirect: 'dashboard' },
+      { path: 'dashboard', component: AdminHome },
+      { path: 'login', component: AdminLogin }
     ]
   },
-  {
-    path: '/web',
-    component: BaseConsumer, // All pages for the new user features should reside here
-    children: [{
-        path: '',
-        component: ConsumerLandingPage
-      },
-      {
-        path: 'country',
-        component: SelectCountry
-      },
-      {
-        path: 'category',
-        component: SelectCategory
-      },
-      {
-        path: 'modal',
-        component: ConsumerModal
-      },
-      {
-        path: 'profile',
-        component: ConsumerProfile
-      },
-      {
-        path: 'trending',
-        component: ConsumersTrending
-      }
+  { path: '/web', component: BaseConsumer, // All pages for the new user features should reside here
+    children: [
+      { path: '', component: ConsumerLandingPage },
+      { path: 'country', component: SelectCountry },
+      { path: 'category', component: SelectCategory },
+      { path: 'modal', component: ConsumerModal },
+      { path: 'profile', component: ConsumerProfile},
+      { path: 'trending', component: ConsumersTrending }
     ]
   },
-  {
-    path: '/login',
-    component: ConsumerSignIn
-  },
-  {
-    path: "*",
-    component: NotFound
-  }
+  { path: '/login', component: ConsumerSignIn },
+  { path: "*", component: NotFound }
 ]
 
 
