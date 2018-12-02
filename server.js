e2abad4fb95239148fd258248f0475751afabd8b
John Olubori David <daveholuborhee@gmail.com>
2018-12-02 10:57:58 +0100

    rewrite post.html

--- server.js
+++ server.js
@@ -1,32 +1,27 @@
 var express = require('express');
 var path = require('path');
 var serveStatic = require('serve-static');
-var history = require('connect-history-api-fallback');
+//var history = require('connect-history-api-fallback');
 // var routes = require('./routes/post');
-var router = express.Router();
+//var router = express.Router();
 let app;
 
 app = express();
 // app.use(history());
 // app.use('/post', routes);
 
-// app.use(express.static(__dirname + '/dist'));
 app.use(serveStatic(path.join(__dirname, '/dist'), {
-    "index": false
+  "index": false
 }))
 
 app.get('/posts/:slug', function (req, res) {
-    res.sendFile(__dirname + '/dist/post.html')
-
+  res.sendFile(__dirname + '/dist/post.html')
 })
+
 app.get('*', function (req, res) {
-    res.sendFile(__dirname + '/dist/index.html')
+  res.sendFile(__dirname + '/dist/index.html')
 
 })
-// // app.get('*', function (req, res) {
-// //     res.sendFile('index.html')
-
-// })
 
 var port = process.env.PORT || 5001;
 app.listen(port);
