9b31b9dc0a842eb8e32f5ed1bbd193ca695f41a0
John Olubori David <daveholuborhee@gmail.com>
2018-12-03 01:49:26 +0100

    finsih social sharing

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var path = require('path');
 var serveStatic = require('serve-static');
+var axios = require('axios')
+var moment = require('moment')
+require('dotenv').config()
 
 //var history = require('connect-history-api-fallback');
 
@@ -9,13 +12,31 @@ let app;
 app = express();
 //app.use(history());
 
+app.set('views', './dist')
+app.set('view engine', 'pug')
 
 app.use(serveStatic(path.join(__dirname, '/dist'), {
   "index": false
 }))
 
-app.get('/redirect/:uri', function (req, res) {
-  res.sendFile(__dirname + '/dist/post.html')
+app.get('/redirect/:uri', async function (req, res) {
+
+  let post, keypointString;
+  try{
+    const resp = await axios.get(`${process.env.VUE_APP_API}posts?slug=${req.params.uri}`)
+    post = resp.data.data && resp.data.data.posts[0];
+
+    let { keypoint, title, slug, image_url, duration, location, device_type } = post;
+    if(keypoint)
+      keypointString = keypoint.join('. ')
+    else
+      keypointString = `Captured ${moment(duration, "YYYY-MM-DD").fromNow()} in ${location} using a ${device_type}`
+
+    res.render('post', { baseUrl: process.env.VUE_APP_URL, keypointString, title, slug, image_url })
+  }catch(err){
+    console.log('Something went wrong with fetching post and rendering') // eslint-disable-line no-console
+    return;
+  }
 })
 
 app.get('*', function (req, res) {
