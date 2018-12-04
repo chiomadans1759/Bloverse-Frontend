520a20e0145242a6f2d230915330bb47022758c5
Vanessa Ating <atingenkay@gmail.com>
2018-12-04 18:46:37 +0100

    chore(unit-test) set up unit test - add the test for app file

---
+++ src/__tests__/App.spec.js
@@ -0,0 +1,10 @@
+import { shallowMount } from '@vue/test-utils';
+import App from '../App';
+import Loading from '../components/Loading';
+
+describe('App Component', () => {
+  it('should render correctly', () => {
+    const wrapper = shallowMount(App, Loading, {});
+    expect(wrapper.classes()).toContain('demo-spin-col');
+  })
+});
