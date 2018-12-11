97e62ad4fc963bd6a95c3fc4a3f6efb742a64546
Habeeb Mac-iver <habeeb.maciver@aiesec.net>
2018-12-11 08:29:23 +0100

    Added dropdown to user profile on header

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -8,13 +8,39 @@
           </router-link>
         </div>
 
-        <div class="col-auto"></div>
-
         <div class="col-auto">
-          <login-button class="pt-2 mr-3" />
-          <router-link to="/creators" class="btn btn-primary btn-md text-uppercase">
-            create content
-          </router-link>
+          <div v-if="!auth.loggedInUser">
+            <login-button class="pt-2 mr-3" />
+            <router-link to="/creators" class="btn btn-primary btn-md text-uppercase">
+              create content
+            </router-link>
+          </div>
+
+          <div v-else>
+            <div class="row">
+              <div class="col">
+                <div class="avatar avatar-sm">
+                  <img :src="auth.loggedInUser.imageUrl" alt="Author's Image" class="avatar-img rounded">
+                </div>
+              </div>
+
+              <div class="col-auto pt-3 pl-0">
+                <div class="dropdown">
+                  <a href="#" class="text-dark" style="font-size: 14px;" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    {{auth.loggedInUser.firstName}} {{auth.loggedInUser.lastName}} <i class="fa fa-caret-down"></i>
+                  </a>
+
+                  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                    <a class="dropdown-item" href="#">Dashboard</a>
+                    <a class="dropdown-item" href="#">Create Post</a>
+                    <a class="dropdown-item" href="#">Your Posts</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">Logout</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -23,15 +49,13 @@
 
 <script>
 import { Row, Col } from 'iview';
-import LoginButton from "./LoginButton"
+import { mapState } from "vuex";
+import LoginButton from "./LoginButton";
 
 export default {
   components: { Row, Col, LoginButton },
   computed: {
-    currentRoute() {
-      let route = this.$route.name
-      return route
-    }
+    ...mapState(["auth"])
   }
 }
 </script>
