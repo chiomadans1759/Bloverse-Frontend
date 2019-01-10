00f79c4f7e4ef13afc922ba16d23367ee565a736
Vanessa Ating <atingenkay@gmail.com>
2019-01-10 20:48:32 +0100

    feat(authentication): fix console errors when running test

--- src/__tests__/components/SigninSection/SigninSection.spec.js
+++ src/__tests__/components/SigninSection/SigninSection.spec.js
@@ -11,5 +11,21 @@ describe('SigninComponent Component', () => {
     });
     expect(wrapper.find(RouterLinkStub).props().to).toBe('/creators/apply')
     expect(wrapper.element).toMatchSnapshot();
+  });
+
+  it('should update the component state when input is changed', () => {
+    const wrapper = mount(SigninComponent, {
+      stubs: {
+        RouterLink: RouterLinkStub
+      }
+    });
+    const emailInput = wrapper.find('#email');
+    emailInput.element.value = 'Janet@email.com';
+    emailInput.trigger('change');
+    const passwordInput = wrapper.find('#password');
+    passwordInput.element.value = 'awesome';
+    passwordInput.trigger('change');
+    expect(wrapper.vm.email).toBe('Janet@email.com');
+    expect(wrapper.vm.password).toBe('awesome');
   })
 });
