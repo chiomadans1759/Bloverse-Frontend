d870a4e93018e716b08cc71fe00dd9a46ba44d65
Adenekan <adenekanwonderful41@gmail.com>
2019-01-14 11:24:19 +0100

    Forgot Password V2 UI

--- src/components/SigninSection/forgot.vue
+++ src/components/SigninSection/forgot.vue
@@ -1,58 +1,69 @@
+
 <template>
-    <div>
-      <!-- <h2 class="welcome-text">Welcome back</h2> -->
-      <!-- <br/> -->
-      <!-- <h2 class="login-text">Login With</h2> -->
-      
-      <!-- <div class="social-login">
-        <Button>
-          <img src="@/assets/facebook-logo-light.svg" />
-        </Button>
-        <br/>
-        <Button>
-          <img src="@/assets/google-text-logo.png" height="25" width="60" />
-        </Button>
-      </div>
-      <br />
-      <p>Or</p>
-      <br /> -->
-      <div class="basic-login">
-      <h2 class="welcome-text">Forgot Password</h2>
+    <div class="signinSection">
+     <h2 class="welcome-text">Forgot Password</h2>
       <br/>
-      <h2 class="login-text">Enter your email address and we will send you a link t reset your password</h2>
-      <br/><br/>
-        <input type="email" placeholder="Email" class="input" />
+      <h2 class="login-text">Enter your email address and <br> we will send you a link t reset your password</h2>
+      <br/>
+    
+      <div class="basic-login">
+        <text-input
+          id="email" name="email" type="email" placeholder="Email" 
+          :value="email" :onChange="onChange" custom-class="text-input" />
         <br/><br/>
-        <!-- <input type="password" placeholder="Password" class="input" />
-        <br/><br/> -->
-       <!--  <p class="forgot-password-link">
-          <a>Forgot password ?</a>
-        </p> -->
-        <br/>
         <Button customClass="login-button">Send</Button>
-        <br/><br/>
+        <br/>
+        <div class="grid-container">
+          <div class="grid-item">
+            <hr class="hr-text" />
+          </div>
+          <div class="middle-grid-item"><p>Or</p></div>
+          <div class="grid-item">
+            <hr class="hr-text" />
+          </div>
+        </div>
+        <br>
         <Button customClass="signup-button">
-          <h2 class="new-bloverse-text">New to bloverse? <span>Sign Up</span></h2>
+          <h2 class="new-bloverse-text">Create <span>new Account</span></h2>
         </Button>
       </div>
       <br/><br/>
-      <h2 class="creator-text">Content creator? 
-        <router-link to="/creators/apply" class="router-link apply-now-text">
-          Apply now
-        </router-link>
-      </h2>
+  
     </div>
 </template>
 <script>
 import Button from '../button/button.vue';
+import TextInput from '../TextInput/TextInput.vue';
 
 export default {
   name: 'SigninComponent',
+  data() {
+    return {
+      email: '',
+      password: ''
+    }
+  },
   components: {
     Button,
+    TextInput
+  },
+  methods: {
+    onChange: function(event) {
+      this[event.target.name] = event.target.value;
+    }
   }
 }
 </script>
 <style lang="scss">
   @import './style.scss';
+</style>
+<style scoped="true">
+  .welcome-text {
+    font-size: 1.5rem;
+    font-weight: 900 !important;
+}
+.login-text {
+    font-size: 0.9rem !important;
+    line-height: 23px;
+}
 </style>
