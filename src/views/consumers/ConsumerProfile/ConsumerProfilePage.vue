b1a3eea73761028ac76b201bc529bf84befa8891
Annmary <annmaryamaka@gmail.com>
2019-01-17 22:45:43 +0100

    ft(terms-and-condition): implements terms and conditions page
    - implement terms and conditons page
    - implement test
    
    [Finishes 412]

---
+++ src/views/consumers/ConsumerProfile/ConsumerProfilePage.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container pt-5">
+    <div class="row">
+      <div class="col-lg-5">left</div>
+      <div class="col-lg-7">right</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped lang="scss">
+  @import './ConsumerProfilePage';
+</style>
