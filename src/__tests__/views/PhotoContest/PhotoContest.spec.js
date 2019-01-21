02d02c8aede9935f3f7510f4b3104479a6e80f8c
Vanessa Ating <atingenkay@gmail.com>
2019-01-21 16:11:18 +0100

    chore(test): fix failing test on staging branch

--- src/__tests__/views/PhotoContest/PhotoContest.spec.js
+++ src/__tests__/views/PhotoContest/PhotoContest.spec.js
@@ -1,5 +1,5 @@
 import { mount, RouterLinkStub  } from '@vue/test-utils'
-import PhotoContest from '@/views/general/photocontest/photocontest.vue';
+import PhotoContest from '@/views/general/PhotoContest/PhotoContestPage.vue';
 
 describe('PhotoContest Component', () => {
   it('should render correctly', () => {
