1ecc241ed5dc09db0d5886fcf190911798633f7f
Daltimore <obinnaleo78@gmail.com>
2019-02-12 04:04:23 +0100

    creators publich page

---
+++ src/__tests__/views/PublishPage/PublishPage.spec.js
@@ -0,0 +1,41 @@
+import { mount, RouterLinkStub } from '@vue/test-utils';
+import PublishPage from '@/views/journalists/Publish/PublishPage.vue';
+
+describe('PublishPage Component', () => {
+  it('should render correctly', () => {
+    const wrapper = mount(PublishPage, {
+      stubs: {
+        RouterLink: RouterLinkStub
+      }
+    });
+    expect(wrapper.element).toMatchSnapshot();
+  });
+
+  it('should increase the `textArea`', () => {
+    const wrapper = mount(PublishPage, {
+      stubs: { RouterLink: RouterLinkStub }
+    });
+    wrapper.find('.show').trigger('click');
+
+    expect(wrapper.vm.textArea).toBe(true);
+  });
+
+  it('should display `showMe`', () => {
+    const wrapper = mount(PublishPage, {
+      stubs: { RouterLink: RouterLinkStub }
+    });
+    wrapper.find('.text').trigger('click');
+
+    expect(wrapper.vm.showMe).toBe(false);
+  });
+
+  it('should display `showThis`', () => {
+    const wrapper = mount(PublishPage, {
+      stubs: { RouterLink: RouterLinkStub }
+    });
+    wrapper.find('.btn').trigger('input');
+    wrapper.find('.nav-link').trigger('input');
+
+    expect(wrapper.vm.showThis).toBe(false);
+  })
+})
