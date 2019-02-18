df530ebf166449b9143f18fe04ca8936b5846af9
Daltimore <obinnaleo78@gmail.com>
2019-02-18 19:48:26 +0100

    implement feedbacks on publish post page

--- src/views/journalists/Publish/PublishPage.vue
+++ src/views/journalists/Publish/PublishPage.vue
@@ -1,19 +1,19 @@
 <template>
   <main id="publish-page">
     <nav class="nav container d-flex justify-content-between align-items-center publish-nav">
-      <a class="nav-link" @click.prevent="goBack">
+      <a class="nav-link" @click="goBack">
          <i class="fal fa-arrow-left back-icon"></i>
          <span class="text-muted ml-2">Back</span>
       </a>
       <p class="nav-link text-uppercase small" v-if="showThis">saving... to draft</p>
-      <button class="btn btn-sm btn-primary-outline preview" @click.prevent="showPreview" v-if="showThis">Preview to post</button>
+      <button class="btn btn-sm btn-primary-outline preview" @click="showPreview" v-if="showThis">Preview to post</button>
     </nav> 
      <div class="container">
        <div class="row">
         <div class="col-lg-1 icon-wrapper mt-4">
           <ul class="text-icons">
             <li>
-              <button class="btn rounded-circle d-flex justify-content-center show" v-if="hideMe" @click.prevent="showTextBox">
+              <button class="btn rounded-circle d-flex justify-content-center show" v-if="hideMe" @click="showTextBox">
                 <i class="fal fa-font"></i>
               </button>
             </li>
@@ -88,9 +88,14 @@
             <div class="form-group d-flex" v-for="(line, index) in lines" :key="index">
               <input type="text" class="form-control p-0 two" placeholder="Add keypoint" v-model="line.keypoint">
               <div>
-                <span v-if="plus_btn">
-                <button class="btn rounded-circle d-flex justify-content-center ml-3 text" @click.prevent="addKeypoint" v-if="line.hideBtn">
+                <span v-if="line.hideBtn">
+                <button class="btn rounded-circle d-flex justify-content-center ml-3 text" @click="addKeypoint" v-if="plus_btn">
                   <i class="fal fa-plus"></i>
+                </button>
+                </span>
+                <span v-if="!line.hideBtn">
+                <button class="btn rounded-circle d-flex justify-content-center ml-3 text" @click="removeKeypoint(index)">
+                  <i class="fal fa-minus"></i>
                 </button>
                 </span>
               </div>
@@ -100,56 +105,62 @@
             </div>
           </form>
         </div>
-        <div class="col-lg-3 err-alert" v-if="showMe">
-          <div class="d-flex justify-content-end mb-4">
-            <button class="btn rounded-circle d-flex justify-content-center text" @click.prevent="closeEntitySection">
-              <i class="fal fa-arrow-right"></i>
-            </button>
+        <div class="col-lg-3 err-alert">
+          <div class="d-flex justify-content-end mb-4 toggle" v-if="showMe" @click="toggleEntitySection">
+            <span>
+              Hide EntitySection
+            </span>
+          </div> 
+          <div class="d-flex justify-content-end mb-4 toggle" v-if="hideEntity"  @click="toggleEntitySection">
+            <span>
+              Show EntitySection
+            </span>
           </div>
-          <error-alert>
+
+          <error-alert v-if="showMe">
             <p>We have 3 articles that have similar title as yours. <a class="compare">Compare</a></p>
           </error-alert>
-          <h6 class="my-4">Entities <span class="badge badge-primary">50</span></h6>
-          <entity-section></entity-section>
+          <h6 v-if="showMe" class="my-4">Entities <span class="badge badge-primary">50</span></h6>
+          <entity-section v-if="showMe"></entity-section>
         </div>
        </div>
       </div>
 
 
-      <!-- Publish Modal -->
-            <div class="modal" id="previewModal" tabindex="-1" role="dialog" aria-hidden="true">
-              <div class="modal-dialog modal-full" role="document">
-                <div class="modal-content" v-if="content1">
-                  <div class="modal-body">
-                    <div>
-                      <div class="loading-dots">
-                      <div class="loading-dots--dot"></div>
-                      <div class="loading-dots--dot"></div>
-                      <div class="loading-dots--dot"></div>
-                    </div>
-                    <p>Scanning Article For Plagiarism...</p>
-                    </div>
-                  </div>
-                </div>
+  <!-- Publish Modal -->
+    <div class="modal" id="previewModal" tabindex="-1" role="dialog" aria-hidden="true">
+      <div class="modal-dialog modal-full" role="document">
+        <div class="modal-content" v-if="content1">
+          <div class="modal-body">
+            <div>
+              <div class="loading-dots">
+              <div class="loading-dots--dot"></div>
+              <div class="loading-dots--dot"></div>
+              <div class="loading-dots--dot"></div>
+            </div>
+            <p>Scanning Article For Plagiarism...</p>
+            </div>
+          </div>
+        </div>
 
-                <div class="modal-content" v-else>
-                  <div class="modal-body">
-                    <div style="width: 45%">
-                    <error-alert1>
-                      <div class="d-flex justify-content-between">
-                        <i class="fal fa-exclamation-circle exclam"></i>
-                          <p>We detected some traces of plagiarism in this content. At bloverse we try to encourage unique and original content. <a class="compare">Compare</a></p>
-                      </div>
-                    </error-alert1>
-                      <div class="click" @click.prevent="toArticle">
-                        <i class="fal fa-arrow-left back-icon"></i>
-                        <span class="ml-2" style="color: #252525;">Return to article</span>
-                      </div>
-                    </div>
-                  </div>
-                </div>
+        <div class="modal-content" v-else>
+          <div class="modal-body">
+            <div style="width: 45%">
+            <error-alert1>
+              <div class="d-flex justify-content-between">
+                <i class="fal fa-exclamation-circle exclam"></i>
+                  <p>We detected some traces of plagiarism in this content. At bloverse we try to encourage unique and original content. <a class="compare">Compare</a></p>
+              </div>
+            </error-alert1>
+              <div class="click" @click="toArticle">
+                <i class="fal fa-arrow-left back-icon"></i>
+                <span class="ml-2" style="color: #252525;">Return to article</span>
               </div>
             </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </main>
 </template>
 
@@ -165,9 +176,10 @@ export default {
   data() {
     return {
       textArea: false,
-      showMe: false,
       textLength: "",
       showThis: false,
+      showMe: false,
+      hideEntity: false,
       hideMe: true,
       content1: true,
       blurBackground: false,
@@ -187,6 +199,16 @@ export default {
       }else{
         this.blurBackground = true
       }
+    },
+    showMe: function(value) {
+      if (value == true ){
+        this.hideEntity = false
+      }else if (value == false && this.textLength.length < 1){
+        this.hideEntity = false
+      }
+      else {
+        this.hideEntity = true
+      }
     }
   },
   methods: {
@@ -198,6 +220,8 @@ export default {
       } else {
         this.showMe = false;
         this.showThis = false;
+        this.hideEntity = false
+
       }
       
     },
@@ -205,8 +229,8 @@ export default {
       this.textArea = true;
       this.$nextTick(() => this.$refs.autoText.focus())
     },
-    closeEntitySection() {
-      this.showMe = false
+    toggleEntitySection() {
+      this.showMe = !this.showMe;
     },
     goBack() {
       this.$router.go(-1);
@@ -216,7 +240,7 @@ export default {
       $('#previewModal').modal('show');
       this.hideMe = false;
 
-      setTimeout(()=>{
+      setTimeout(() => {
         this.content1 = false
       }, 3000)
     },
@@ -235,6 +259,12 @@ export default {
 
       if(this.lines.length == 3){
         this.plus_btn = false;
+      }
+    },
+    removeKeypoint(index) {
+      this.lines.splice(index,1)
+      if (this.lines.length == 1) {
+        this.plus_btn = true
       }
     }
   }
